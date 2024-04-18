d89e983652f8b705b91d6d53291833950a947b5d
Erik Stein <erik.stein.2876@student.lu.se>
2024-04-18 11:00:12 +0200

    indentation

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((c-mode . ((indent-tabs-mode . t)
+	    (c-basic-offset . 8)
+	    (tab-width . 8))))
--- getting_started.c
+++ getting_started.c
@@ -44,134 +44,134 @@ uint32_t get_micros(); // prototype to get elapsed time in microseconds
 
 int main(int argc, char** argv)
 {
-    radio = new_rf24(CE_PIN, CSN_PIN);
+	radio = new_rf24(CE_PIN, CSN_PIN);
 
-    // perform hardware check
-    /* if (!rf24_begin(radio)) { */
-    /*   printf("radio hardware is not responding!!\n"); */
-    /*   return 0; // quit now */
-    /* } */
-    rf24_begin(radio);
+	// perform hardware check
+	/* if (!rf24_begin(radio)) { */
+	/*   printf("radio hardware is not responding!!\n"); */
+	/*   return 0; // quit now */
+	/* } */
+	rf24_begin(radio);
 
-    rf24_setChannel(radio, CHANNEL);
+	rf24_setChannel(radio, CHANNEL);
 
-    // to use different addresses on a pair of radios, we need a variable to
-    // uniquely identify which address this radio will use to transmit
-    int radio_number = 1; // 0 uses address[0] to transmit, 1 uses address[1] to transmit
+	// to use different addresses on a pair of radios, we need a variable to
+	// uniquely identify which address this radio will use to transmit
+	int radio_number = 1; // 0 uses address[0] to transmit, 1 uses address[1] to transmit
 
-    // print example's name
-    printf("%s\n", argv[0]);
+	// print example's name
+	printf("%s\n", argv[0]);
 
-    // Let these addresses be used for the pair
-    char address[2][6] = {"1Node", "2Node"};
-    // It is very helpful to think of an address as a path instead of as
-    // an identifying device destination
+	// Let these addresses be used for the pair
+	char address[2][6] = {"1Node", "2Node"};
+	// It is very helpful to think of an address as a path instead of as
+	// an identifying device destination
 
-    // Set the radioNumber via the terminal on startup
-    printf("Which radio is this? Enter '0' or '1'. ");
-    int input;
-    scanf("%d", &radio_number);
+	// Set the radioNumber via the terminal on startup
+	printf("Which radio is this? Enter '0' or '1'. ");
+	int input;
+	scanf("%d", &radio_number);
 
-    // save on transmission time by setting the radio to only transmit the
-    // number of bytes we need to transmit a float
-    rf24_setPayloadSize(radio, sizeof(payload)); // float datatype occupies 4 bytes
+	// save on transmission time by setting the radio to only transmit the
+	// number of bytes we need to transmit a float
+	rf24_setPayloadSize(radio, sizeof(payload)); // float datatype occupies 4 bytes
 
-    // Set the PA Level low to try preventing power supply related problems
-    // because these examples are likely run with nodes in close proximity to
-    // each other.
-    rf24_setPALevel(radio, RF24_PA_LOW); // RF24_PA_MAX is default.
+	// Set the PA Level low to try preventing power supply related problems
+	// because these examples are likely run with nodes in close proximity to
+	// each other.
+	rf24_setPALevel(radio, RF24_PA_LOW); // RF24_PA_MAX is default.
 
-    // set the TX address of the RX node into the TX pipe
-    rf24_openWritingPipe(radio, address[radio_number]); // always uses pipe 0
+	// set the TX address of the RX node into the TX pipe
+	rf24_openWritingPipe(radio, address[radio_number]); // always uses pipe 0
 
-    // set the RX address of the TX node into a RX pipe
-    rf24_openReadingPipe(radio, 1, address[!radio_number]); // using pipe 1
+	// set the RX address of the TX node into a RX pipe
+	rf24_openReadingPipe(radio, 1, address[!radio_number]); // using pipe 1
 
-    // For debugging info
-    // rf24_printDetails(radio);       // (smaller) function that prints raw register values
-    rf24_printPrettyDetails(radio); // (larger) function that prints human readable data
+	// For debugging info
+	// rf24_printDetails(radio);       // (smaller) function that prints raw register values
+	rf24_printPrettyDetails(radio); // (larger) function that prints human readable data
 
-    // ready to execute program now
-    set_role(); // calls master() or slave() based on user input
-    return 0;
+	// ready to execute program now
+	set_role(); // calls master() or slave() based on user input
+	return 0;
 }
 
 void set_role()
 {
-    int input = -1;
-    while (input == -1) {
-      printf("*** PRESS '1' to begin transmitting to the other node\n");
-      printf("*** PRESS '2' to begin receiving from the other node\n");
-      printf("*** PRESS '0' to exit\n");
-      scanf("%d", &input);
-      if (input >= 0) {
-	if (input == 1)
-	  master();
-	else if (input == 2)
-	  slave();
-	else if (input == 0)
-	  break;
-	else
-	  printf("%d is an invalid input. Please try again.\n", input);
-      }
-      input = -1; // stay in the while loop
-    }               // while
+	int input = -1;
+	while (input == -1) {
+		printf("*** PRESS '1' to begin transmitting to the other node\n");
+		printf("*** PRESS '2' to begin receiving from the other node\n");
+		printf("*** PRESS '0' to exit\n");
+		scanf("%d", &input);
+		if (input >= 0) {
+			if (input == 1)
+				master();
+			else if (input == 2)
+				slave();
+			else if (input == 0)
+				break;
+			else
+				printf("%d is an invalid input. Please try again.\n", input);
+		}
+		input = -1; // stay in the while loop
+	}               // while
 } // setRole()
 
 void master()
 {
-    rf24_stopListening(radio); // put radio in TX mode
+	rf24_stopListening(radio); // put radio in TX mode
 
-    unsigned int failure = 0; // keep track of failures
-    while (failure < 6) {
-        clock_gettime(CLOCK_MONOTONIC_RAW, &start_timer);    // start the timer
-	int report = rf24_write(radio, &payload, sizeof(float)); // transmit & save the report
-        uint32_t timer_elapsed = get_micros();                // end the timer
+	unsigned int failure = 0; // keep track of failures
+	while (failure < 6) {
+		clock_gettime(CLOCK_MONOTONIC_RAW, &start_timer);    // start the timer
+		int report = rf24_write(radio, &payload, sizeof(float)); // transmit & save the report
+		uint32_t timer_elapsed = get_micros();                // end the timer
 
-        if (report) {
-	  // payload was delivered
-	  printf("Transmission successful! Time to transmit = %ld us. Sent: %f\n", timer_elapsed, payload);
-	  payload += 0.01;                          // increment float payload
-        }
-        else {
-            // payload was not delivered
-	  printf("Transmission failed or timed out\n");
-	  failure++;
-        }
+		if (report) {
+			// payload was delivered
+			printf("Transmission successful! Time to transmit = %ld us. Sent: %f\n", timer_elapsed, payload);
+			payload += 0.01;                          // increment float payload
+		}
+		else {
+			// payload was not delivered
+			printf("Transmission failed or timed out\n");
+			failure++;
+		}
 
-        // to make this example readable in the terminal
-        sleep(1); // slow transmissions down by 1 second
-    }
-    printf("%d failures detected. Leaving TX role.\n", failure);
+		// to make this example readable in the terminal
+		sleep(1); // slow transmissions down by 1 second
+	}
+	printf("%d failures detected. Leaving TX role.\n", failure);
 }
 
 void slave()
 {
 
-    rf24_startListening(radio); // put radio in RX mode
+	rf24_startListening(radio); // put radio in RX mode
 
-    time_t start_timer = time(NULL);       // start a timer
-    while (time(NULL) - start_timer < 6) { // use 6 second timeout
-        uint8_t pipe;
-        if (rf24_available_pipe(radio, &pipe)) {                        // is there a payload? get the pipe number that recieved it
-            uint8_t bytes = rf24_getPayloadSize(radio);          // get the size of the payload
-            rf24_read(radio, &payload, bytes);                     // fetch payload from FIFO
-            printf("Received %d bytes on pipe %d: %f\n", bytes, pipe, payload);      // print the size of the payload
-            start_timer = time(NULL);                      // reset timer
-        }
-    }
-    printf("Nothing received in 6 seconds. Leaving RX role.\n");
-    rf24_stopListening(radio);
+	time_t start_timer = time(NULL);       // start a timer
+	while (time(NULL) - start_timer < 6) { // use 6 second timeout
+		uint8_t pipe;
+		if (rf24_available_pipe(radio, &pipe)) {                        // is there a payload? get the pipe number that recieved it
+			uint8_t bytes = rf24_getPayloadSize(radio);          // get the size of the payload
+			rf24_read(radio, &payload, bytes);                     // fetch payload from FIFO
+			printf("Received %d bytes on pipe %d: %f\n", bytes, pipe, payload);      // print the size of the payload
+			start_timer = time(NULL);                      // reset timer
+		}
+	}
+	printf("Nothing received in 6 seconds. Leaving RX role.\n");
+	rf24_stopListening(radio);
 }
 
 uint32_t get_micros()
 {
-    // this function assumes that the timer was started using
-    // `clock_gettime(CLOCK_MONOTONIC_RAW, &startTimer);`
+	// this function assumes that the timer was started using
+	// `clock_gettime(CLOCK_MONOTONIC_RAW, &startTimer);`
 
-    clock_gettime(CLOCK_MONOTONIC_RAW, &end_timer);
-    uint32_t seconds = end_timer.tv_sec - start_timer.tv_sec;
-    uint32_t useconds = (end_timer.tv_nsec - start_timer.tv_nsec) / 1000;
+	clock_gettime(CLOCK_MONOTONIC_RAW, &end_timer);
+	uint32_t seconds = end_timer.tv_sec - start_timer.tv_sec;
+	uint32_t useconds = (end_timer.tv_nsec - start_timer.tv_nsec) / 1000;
 
-    return ((seconds)*1000 + useconds) + 0.5;
+	return ((seconds)*1000 + useconds) + 0.5;
 }
