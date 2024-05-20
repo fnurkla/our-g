a5fe456abb4d27ca82ddf1653e9b838cd7045a84
Erik Stein <erik.stein.2876@student.lu.se>
2024-05-20 14:48:39 +0200

    Small changes and optimizations
    * Change char to uint8_t
    * Only query payload size once and use that value forever
    * Don't retry transmissions, let the autoretransmit and tcp handle that

--- tun_nrf.c
+++ tun_nrf.c
@@ -33,11 +33,12 @@
 
 #define VIRTUAL_INTERFACE "tun0"
 #define BUFLEN 65535
-#define MAX_RETRY 5
 #define MTU 1500
 
 struct timespec delay = {0, 50000}; // 50 µs
 struct timespec send_delay = {0, 500000}; // 500 µs
+
+uint8_t payload_size;
 
 
 RF24Handle make_radio(int ce_pin, int csn_pin, int channel, int is_receiver) {
@@ -50,10 +51,12 @@ RF24Handle make_radio(int ce_pin, int csn_pin, int channel, int is_receiver) {
 	rf24_openWritingPipe(radio, address[is_receiver]);
 	rf24_openReadingPipe(radio, 1, address[!is_receiver]);
 
+	payload_size = rf24_getPayloadSize(radio);
+
 	return radio;
 }
 
-int is_good_ip_header(char *buf) {
+int is_good_ip_header(uint8_t *buf) {
 	// Ip version
 	if ((buf[0] & 0b11110000) != (4 << 4)) {
 		pr("Not beginning of ipv4 packet.\n");
@@ -73,18 +76,16 @@ int is_good_ip_header(char *buf) {
 	return 1;
 }
 
-size_t listen_and_defragment(RF24Handle radio, char* buffer) {
-	uint8_t bytes;
+size_t listen_and_defragment(RF24Handle radio, uint8_t* buffer) {
 	int total_length;
 	if (rf24_available(radio)) {
-		bytes = rf24_getPayloadSize(radio);
-		rf24_read(radio, buffer, bytes);
+		rf24_read(radio, buffer, payload_size);
 
 		if (!is_good_ip_header(buffer)) {
 			pr("Discarding.\n");
 			return 0;
 		}
-		total_length = (buffer[2] << 8) + buffer[3];
+		total_length = (buffer[2] << 8) | buffer[3];
 		if (total_length > MTU) {
 			pr("Incoming package (%d) longer than %d bytes, discarding.\n", total_length, MTU);
 			return 0;
@@ -95,36 +96,26 @@ size_t listen_and_defragment(RF24Handle radio, char* buffer) {
 	}
 
 	int i;
-	for (i = bytes; i < total_length; i += bytes) {
+	for (i = payload_size; i < total_length; i += payload_size) {
 		while (!rf24_available(radio)) {
 			nanosleep(&delay, NULL);
 		}
 
-		bytes = rf24_getPayloadSize(radio);
-		if (i + bytes > BUFLEN) {
+		if (i + payload_size > BUFLEN) {
 			pr("Buffer full");
 			return i;
 		}
-		rf24_read(radio, &buffer[i], bytes);
+		rf24_read(radio, buffer + i, payload_size);
 		pr("have received %d/%d bytes\n", i, total_length);
 	}
 	return total_length;
 }
 
-void fragment_and_send(RF24Handle radio, char* payload, ssize_t size) {
+void fragment_and_send(RF24Handle radio, uint8_t* payload, ssize_t size) {
 	for (int i = 0; i < size; i += 32) {
-		char* bytes = &payload[i];
+		uint8_t* bytes = payload + i;
 		size_t cur_size = size - i < 32 ? size - i : 32;
-		int success = 0;
-		int tries = 0;
-		while (!success && tries < MAX_RETRY) {
-			tries++;
-			success = rf24_write(radio, bytes, cur_size);
-		}
-		if (tries == MAX_RETRY) {
-			pr("Max retries reached\n");
-			return;
-		}
+		rf24_write(radio, bytes, cur_size);
 		nanosleep(&send_delay, NULL);
 	}
 }
@@ -133,7 +124,7 @@ void *do_receive(void *argument) {
 	int tun_fd = *((int *) argument);
 
 	RF24Handle radio = make_radio(RX_CE_PIN, RX_CSN_PIN, RX_CHANNEL, 1);
-	char buf[BUFLEN];
+	uint8_t buf[BUFLEN];
 
 	rf24_startListening(radio);
 
@@ -150,7 +141,7 @@ void *do_send(void *argument) {
 	int tun_fd = *((int *) argument);
 
 	RF24Handle radio = make_radio(TX_CE_PIN, TX_CSN_PIN, TX_CHANNEL, 0);
-	char buf[BUFLEN];
+	uint8_t buf[BUFLEN];
 
 	rf24_stopListening(radio);
 	while (1) {
